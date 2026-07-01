fd80889a8e00add7e524c4b92bbda508ee8e7942
TomP22 <to.penap@duocuc.cl>
2026-07-01 16:54:30 -0400

    feat(config): implementar configuracion de RestClient con balanceo de carga

--- src/main/java/cl/municipalidad/canchas/config/RestClientConfig.java
+++ src/main/java/cl/municipalidad/canchas/config/RestClientConfig.java
@@ -4,19 +4,30 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
 @Configuration
 public class RestClientConfig {
+
+    private SimpleClientHttpRequestFactory configureTimeouts() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(3000);
+        factory.setReadTimeout(5000);  
+        return factory;
+    }
 
     @Bean
     @LoadBalanced
     public RestClient.Builder loadBalancedRestClientBuilder() {
-        return RestClient.builder();
+        return RestClient.builder()
+                .requestFactory(configureTimeouts());
     }
 
     @Bean
     @Primary
     public RestClient.Builder normalRestClientBuilder() {
-        return RestClient.builder();
+        return RestClient.builder()
+                .requestFactory(configureTimeouts());
     }
 }
